a99dca56fd4eb8bd3ea16d68afaf1d7ce56fe4e3
Zypheus <c25887@gmail.com>
2022-07-14 17:37:54 +0800

    stage5

--- game.py
+++ game.py
@@ -1,5 +1,4 @@
-##插件與腳位定義----------------------------------------------
-from machine import I2C,Pin##
+from machine import I2C,Pin
 from gpb import delay
 import urandom
 
@@ -20,7 +19,7 @@ ble.cmd_mode_entry()
 ble.cmd_AT()
 ble.set_device_name('New Age Men')
 
-
+##元件腳位----------------------------------------------------
 rgbR = Pin(2,Pin.OUT)
 rgbG = Pin(3,Pin.OUT)
 rgbB = Pin(4,Pin.OUT)
@@ -28,19 +27,20 @@ btnR = Pin(11,Pin.IN)
 btnG = Pin(12,Pin.IN)
 btnB = Pin(13,Pin.IN)
 
-
-
-##定義--------------------------------------------------------
-currentStageIndex = 2
-stage2Success = 0
+##全域變數----------------------------------------------------
 timer = 0
+currentStageIndex = 5
+stage2SuccessFlag = 0
+stage5SuccessFlag = 0
 
-rgbRIndex = 0
-rgbGIndex = 0
-rgbBIndex = 0
 color = ""
 
-def clearLCD():
+##函式--------------------------------------------------------
+def clearLCD1():
+    delay(1000)
+    lcd.clear()
+    delay(500)
+def clearLCD2():
     delay(2000)
     lcd.clear()
     delay(500)
@@ -52,8 +52,8 @@ def clearLED():
     
 def startGame():
     lcd.move_to(0,0)
-    lcd.putstr("Game start!!")
-    clearLCD()
+    lcd.putstr("Game Start!!")
+    clearLCD2()
 
 def currentStage(index):
     if index == 1:
@@ -76,23 +76,26 @@ def nextStage():
     global currentStageIndex
     currentStageIndex += 1
     
-    clearLCD()
+    clearLCD1()
     lcd.move_to(0,0)
     lcd.putstr("Stage Clear!!")
-    clearLCD()
+    clearLCD2()
     lcd.putstr("Next Stage")
     lcd.move_to(0,1)
     lcd.putstr("Stage: " + str(currentStageIndex) + "/5")
-    clearLCD()
+    clearLCD2()
 
 ##各關函式----------------------------------------------------
+##Stage1 : 電阻控制風扇
 def stage1LCD():
     lcd.move_to(0,0)
-    lcd.putstr("control the Fan")
+    lcd.putstr("Control The Fan")
     
-##def stage1(): ##電阻控制風扇
+##def stage1():
     
-    
+
+
+##Stage2 顏色: 按鈕控制RGB
 def stage2LCD():
     global color
     
@@ -101,17 +104,16 @@ def stage2LCD():
     color = colorList[picker]
     
     lcd.move_to(0,0)
-    lcd.putstr("change the light")
+    lcd.putstr("Change The Light")
     lcd.move_to(0,1)
     lcd.putstr("to " + color)
     
-def stage2(): ##按鈕RGB
-    global currentStageIndex
-    global stage2Success
+def stage2(): 
+    global stage2SuccessIndex
     global color
-    global rgbRIndex
-    global rgbGIndex
-    global rgbBIndex
+    rgbRIndex = 0
+    rgbGIndex = 0
+    rgbBIndex = 0
     
     while True:
         if btnR.value() == 1:
@@ -141,69 +143,104 @@ def stage2(): ##按鈕RGB
             
         if color == "Yellow":
             if rgbRIndex == 1 & rgbGIndex == 1:
-                stage2Success = 1
+                stage2SuccessIndex = 1
+                delay(1000)
+                clearLED()
                 break
         if color == "Magenta":
             if rgbRIndex == 1 & rgbBIndex == 1:
-                stage2Success = 1
+                stage2SuccessIndex = 1
+                delay(1000)
+                clearLED()
                 break
         if color == "Cyan":
             if rgbGIndex == 1 & rgbBIndex == 1:
-                stage2Success = 1
+                stage2SuccessIndex = 1
+                delay(1000)
+                clearLED()
                 break
         if color == "White":
             if rgbRIndex == 1 & rgbGIndex == 1 & rgbBIndex == 1:
-                stage2Success = 1
+                stage2SuccessIndex = 1
+                delay(1000)
+                clearLED()
                 break
     
-    
+
+##Stage3 : 手指增加濕度(標準為當下濕度，通關為當下+5或3)
 def stage3LCD():
     lcd.move_to(0,0)
     lcd.putstr("3")
     
-##def stage3(): ##手指增加濕度(標準為當下濕度，通關為當下+5或3)
+##def stage3():
     
     
+##Stage4 : 光敏電阻
 def stage4LCD():
     lcd.move_to(0,0)
     lcd.putstr("4")
     
-##def stage4(): ##光敏
+##def stage4(): 
 
 
+##Stage5 藍芽模組解謎: 手機答題
 def stage5LCD():
     lcd.move_to(0,0)
-    lcd.putstr("5")
+    lcd.putstr("Answer on Phone")
     
-def stage5(): ##藍芽模組解謎
+def correct():
+    global stage5SuccessFlag
+
+    clearLCD1()
+    lcd.move_to(0,0)
+    lcd.putstr("Correct")
+    clearLCD1()
+    stage5SuccessFlag = 1
+
+def incorrect():
+    clearLCD1()
+    lcd.move_to(0,0)
+    lcd.putstr("Incorrect")
+    clearLCD1()
+    stage5LCD()
+    
+def stage5():
+    questionList = ["cc","cc2"] ##TODO: 放題目
+    picker = urandom.randint(0, 1)
+    question = questionList[picker]
+    print(question)
+    
     while True:
         cmd = str(ble.read(30), 'utf-8').strip('\0')
-        if cmd != '':
-            if cmd == 'cc':
-                print('cc2')
-            elif cmd == 'lol':
-                print('lol2')
+        if cmd == "SYSTEM-ECHO=CONNECTED OK\r\n": ##學習歷程有得寫囉 嘻嘻
+            delay(2000)
+            ble.write("HAHA")
+        else:
+            if cmd != "Command mode OK\r\n": ##學習歷程又有得寫囉 嘻嘻
+                if cmd != "":
+                    if question == questionList[0]:
+                        if cmd == question:
+                            correct()
+                            break
+                        else:
+                            incorrect()
+                    if question == questionList[1]:
+                        if cmd == question:
+                            correct()
+                            break
+                        else:
+                            incorrect()
         delay(10)
-
-##------------------------------------------------------------
-##def checkStage():
-##    lcd.clear()
-##    delay(500)
-##    lcd.move_to(0,0)
-##    lcd.putstr("Stage: " + str(currentStageIndex) + "/5")
-##    delay(500)
-##    currentStage(currentStageIndex)
-
 
 ##主程式------------------------------------------------------
 startGame()
-'''
-currentStage(2)
-if stage2Success == 1:
-    nextStage()
-    clearLED()
-    currentStage(currentStageIndex)
-print(currentStageIndex)
-'''
 currentStage(5)
+'''
+if stage2SuccessFlag == 1:
+    nextStage()
+    currentStage(currentStageIndex)
+'''
+if stage5SuccessFlag == 1:
+    print("YA")
+
         
