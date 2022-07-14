e677b24c658efa8c4dcef952754b4035760160c9
Zypheus <c25887@gmail.com>
2022-07-14 18:53:57 +0800

    update

--- game.py
+++ game.py
@@ -1,14 +1,11 @@
 ##插件與腳位定義----------------------------------------------
-from machine import I2C,Pin,ADC,PWM
+from machine import I2C, Pin, ADC, PWM
 from gpb import delay
 import urandom
 
 from ble_uart import BleUart
 from i2c_lcd import I2cLcd
 from lcd_api import LcdApi
-
-import math
-
 
 I2C_ADDR = 0x27
 I2C_NUM_ROWS = 2
@@ -23,6 +20,9 @@ ble.cmd_AT()
 ble.set_device_name('New Age Men')
 
 ##元件腳位----------------------------------------------------
+vr = ADC(1)
+##fan = PWM(9, 200, 0)
+
 rgbR = Pin(2,Pin.OUT)
 rgbG = Pin(3,Pin.OUT)
 rgbB = Pin(4,Pin.OUT)
@@ -31,17 +31,10 @@ btnG = Pin(12,Pin.IN)
 btnB = Pin(13,Pin.IN)
 
 ##全域變數----------------------------------------------------
-currentStageIndex = 2
 timer = 0
+currentStageIndex = 1
 
-vr = ADC(1)
-fan = PWM(9, 200, 0)
-stage1Target = 0  ## =0(讓風扇轉到最快) =1(讓風扇停)
-stage1Success = 0
-stage1Sense = 0  #偵測電阻值一次就好
-
-
-currentStageIndex = 5
+stage1SuccessFlag = 0
 stage2SuccessFlag = 0
 stage5SuccessFlag = 0
 
@@ -71,7 +64,6 @@ def currentStage(index):
     if index == 1:
         stage1LCD()
         stage1()
-        ##stage1()
     elif index == 2:
         stage2LCD()
         stage2()
@@ -105,32 +97,33 @@ def stage1LCD():
     lcd.putstr("control the Fan")
     
 def stage1():
+    global stage1SuccessFlag
     global vr
-    global stage1Target
-    global stage1Success
-    global stage1Sense
+    target = 0 ## 0:讓風扇轉到最快, 1:讓風扇停
+    sense = 0 ##只偵測電阻值一次
+    
     while True:
         print(vr.read())
         fan.duty(int(vr.read()/4))
-        if stage1Sense == 0:
-            if vr.read()<2000:
-                stage1Target = 0  ##讓風扇轉到最快
-                stage1Sense = 1
-            if vr.read()>2001:
-                stage1Target = 1  ##讓風扇停
-                stage1Sense = 1
-            
-        if stage1Target == 1:
-            if vr.read() < 70:
-                stage1Success = 1
-                fan.duty(0)
-                break
-        if stage1Target == 0:
-            if vr.read() > 3600 :
-                stage1Success = 1
-                fan.duty(0)
-                break
         
+        if sense == 0:
+            if vr.read() < 2000:
+                target = 0  ##讓風扇轉到最快
+                sense = 1
+            if vr.read()>2001:
+                target = 1  ##讓風扇停
+                sense = 1
+            
+        if target == 1:
+            if vr.read() < 70:
+                stage1SuccessFlag = 1
+                fan.duty(0)
+                break
+        else:
+            if vr.read() > 3600 :
+                stage1SuccessFlag = 1
+                fan.duty(0)
+                break
         delay(100)
 
 ##Stage2 顏色: 按鈕控制RGB
@@ -138,8 +131,10 @@ def stage2LCD():
     global color
     
     colorList = ["Yellow", "Magenta", "Cyan", "White"]
-    picker = urandom.randint(0, 3)
-    color = colorList[picker]
+    colorPicker = urandom.randint(0, 3)
+    color = colorList[colorPicker]
+    print(color)
+    print(colorPicker)
     
     lcd.move_to(0,0)
     lcd.putstr("Change The Light")
@@ -147,7 +142,7 @@ def stage2LCD():
     lcd.putstr("to " + color)
     
 def stage2(): 
-    global stage2SuccessIndex
+    global stage2SuccessFlag
     global color
     rgbRIndex = 0
     rgbGIndex = 0
@@ -181,25 +176,25 @@ def stage2():
             
         if color == "Yellow":
             if rgbRIndex == 1 & rgbGIndex == 1:
-                stage2SuccessIndex = 1
+                stage2SuccessFlag = 1
                 delay(1000)
                 clearLED()
                 break
-        if color == "Magenta":
+        elif color == "Magenta":
             if rgbRIndex == 1 & rgbBIndex == 1:
-                stage2SuccessIndex = 1
+                stage2SuccessFlag = 1
                 delay(1000)
                 clearLED()
                 break
-        if color == "Cyan":
+        elif color == "Cyan":
             if rgbGIndex == 1 & rgbBIndex == 1:
-                stage2SuccessIndex = 1
+                stage2SuccessFlag = 1
                 delay(1000)
                 clearLED()
                 break
-        if color == "White":
+        elif color == "White":
             if rgbRIndex == 1 & rgbGIndex == 1 & rgbBIndex == 1:
-                stage2SuccessIndex = 1
+                stage2SuccessFlag = 1
                 delay(1000)
                 clearLED()
                 break
@@ -244,42 +239,45 @@ def incorrect():
     
 def stage5():
     questionList = ["cc","cc2"] ##TODO: 放題目
-    picker = urandom.randint(0, 1)
-    question = questionList[picker]
+    qPicker = urandom.randint(0, 1)
+    question = questionList[qPicker]
     print(question)
     
     while True:
         cmd = str(ble.read(30), 'utf-8').strip('\0')
-        if cmd == "SYSTEM-ECHO=CONNECTED OK\r\n": ##學習歷程有得寫囉 嘻嘻
-            delay(2000)
-            ble.write("HAHA")
-        else:
-            if cmd != "Command mode OK\r\n": ##學習歷程又有得寫囉 嘻嘻
-                if cmd != "":
-                    if question == questionList[0]:
-                        if cmd == question:
-                            correct()
-                            break
-                        else:
-                            incorrect()
-                    if question == questionList[1]:
-                        if cmd == question:
-                            correct()
-                            break
-                        else:
-                            incorrect()
-        delay(10)
+        if cmd == 'SYSTEM-ECHO=CONNECTED OK\r\n':      ##學習歷程有得寫囉 嘻嘻
+            ble.data_mode_entry()
+            is_ble_connected = True
+        elif cmd == 'SYSTEM-ECHO=DISCONNECTED OK\r\n':
+            ble.cmd_mode_entry()
+            is_ble_connected = False
+        if is_ble_connected == True:
+            delay(100)
+            ble.write("Question")
+            if question == questionList[0]:
+                if cmd == question:
+                    correct()
+                    break
+                else:
+                    incorrect()
+            if question == questionList[1]:
+                if cmd == question:
+                    correct()
+                    break
+                else:
+                    incorrect()
+        delay(1000)
 
 ##主程式------------------------------------------------------
 startGame()
-currentStage(5)
-'''
-if stage1Success == 1:
+currentStage(2)
+
+if stage1SuccessFlag == 1:
     nextStage()
     currentStage(currentStageIndex)
 if stage2SuccessFlag == 1:
     nextStage()
     currentStage(currentStageIndex)
-'''
+
 if stage5SuccessFlag == 1:
     print("YA")       
