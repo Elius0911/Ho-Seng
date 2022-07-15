8e7ee9ed48e2c1f32325b3a37d256611e3b4f758
Zypheus <c25887@gmail.com>
2022-07-15 17:15:02 +0800

    bluetooth_Instruction

--- game.py
+++ game.py
@@ -1,14 +1,11 @@
 ##插件與腳位定義----------------------------------------------
-from machine import I2C,Pin,ADC,PWM
+from machine import I2C, Pin, ADC, PWM
 from gpb import delay
 import urandom
 
 from ble_uart import BleUart
 from i2c_lcd import I2cLcd
 from lcd_api import LcdApi
-
-import math
-
 
 I2C_ADDR = 0x27
 I2C_NUM_ROWS = 2
@@ -22,8 +19,6 @@ ble.cmd_mode_entry()
 ble.cmd_AT()
 ble.set_device_name('New Age Men')
 
-<<<<<<< Updated upstream
-=======
 while True:
     cmd = str(ble.read(30), 'utf-8').strip('\0')
     print(cmd)
@@ -36,8 +31,7 @@ while True:
 
 ##元件腳位----------------------------------------------------
 vr = ADC(1)
-fan = PWM(9, 200, 0)
->>>>>>> Stashed changes
+##fan = PWM(9, 200, 0)
 
 rgbR = Pin(2,Pin.OUT)
 rgbG = Pin(3,Pin.OUT)
@@ -46,23 +40,6 @@ btnR = Pin(11,Pin.IN)
 btnG = Pin(12,Pin.IN)
 btnB = Pin(13,Pin.IN)
 
-<<<<<<< Updated upstream
-vr = ADC(1)
-fan = PWM(9, 200, 0)
-stage1Target = 0  ## =0(讓風扇轉到最快) =1(讓風扇停)
-stage1Success = 0
-stage1Sense = 0  #偵測電阻值一次就好
-
-##定義--------------------------------------------------------
-currentStageIndex = 2
-stage2Success = 0
-timer = 0
-
-rgbRIndex = 0
-rgbGIndex = 0
-rgbBIndex = 0
-color = ""
-=======
 
 ##全域變數----------------------------------------------------
 timer = 0
@@ -82,9 +59,13 @@ s2ColorList = ["Yellow", "Purple", "Cyan", "White"]
 s5QuestionList = ["cc","cc2"]    
 s5Question = ""
 
->>>>>>> Stashed changes
 
-def clearLCD():
+##函式--------------------------------------------------------
+def clearLCD1():
+    delay(1000)
+    lcd.clear()
+    delay(500)
+def clearLCD2():
     delay(2000)
     lcd.clear()
     delay(500)
@@ -96,16 +77,11 @@ def clearLED():
 
 def startGame():
     lcd.move_to(0,0)
-<<<<<<< Updated upstream
-    lcd.putstr("Game start!!")
-    clearLCD()
-=======
     lcd.putstr("Game Start!!")
     lcd.move_to(0,1)
     lcd.putstr("Stage: " + str(currentStageIndex) + "/5")
     ble.write("-------------Stage " + str(currentStageIndex) +"-------------")
     clearLCD2()
->>>>>>> Stashed changes
 
 def bleStageInstructionDisplay(index):
     global stageInstruction
@@ -119,7 +95,6 @@ def currentStage(index):
         stage1Display()
         bleStageInstructionDisplay(currentStageIndex)
         stage1()
-        ##stage1()
     elif index == 2:
         stage2Front()
         stage2Display()
@@ -144,17 +119,6 @@ def currentStage(index):
 def nextStage():
     global currentStageIndex
     currentStageIndex += 1
-<<<<<<< Updated upstream
-    
-    clearLCD()
-    lcd.move_to(0,0)
-    lcd.putstr("Stage Clear!!")
-    clearLCD()
-    lcd.putstr("Next Stage")
-    lcd.move_to(0,1)
-    lcd.putstr("Stage: " + str(currentStageIndex) + "/5")
-    clearLCD()
-=======
 
     clearLCD1()
     lcd.move_to(0,0)
@@ -166,7 +130,6 @@ def nextStage():
     lcd.putstr("Stage: " + str(currentStageIndex) + "/5")
     ble.write("-------------Stage " + str(currentStageIndex) +"-------------")
     clearLCD2()
->>>>>>> Stashed changes
 
 def finish():
     clearLCD1()
@@ -178,9 +141,6 @@ def finish():
 
 
 ##各關函式----------------------------------------------------
-<<<<<<< Updated upstream
-def stage1LCD():
-=======
 
 ##--- Stage1: 電阻控制風扇 ---
 def stage1Front():
@@ -198,7 +158,6 @@ def stage1Display():
     global stageInstruction
     global s1Target
 
->>>>>>> Stashed changes
     lcd.move_to(0,0)
     lcd.putstr("Control the Fan")
 
@@ -209,31 +168,8 @@ def stage1Display():
         stageInstruction[1] = "控制風扇，讓它停下來"
     
 def stage1():
+    global stage1SuccessFlag
     global vr
-<<<<<<< Updated upstream
-    global stage1Target
-    global stage1Success
-    global stage1Sense
-    while True:
-        print(vr.read())
-        fan.duty(int(vr.read()/4))
-        if stage1Sense == 0:
-            if vr.read()<2000:
-                stage1Target = 0  ##讓風扇轉到最快
-                stage1Sense = 1
-            if vr.read()>2001:
-                stage1Target = 1  ##讓風扇停
-                stage1Sense = 1
-            
-        if stage1Target == 1:
-            if vr.read() < 70:
-                stage1Success = 1
-                fan.duty(0)
-                break
-        if stage1Target == 0:
-            if vr.read() > 3600 :
-                stage1Success = 1
-=======
     global s1Target
 
     while True:
@@ -247,24 +183,9 @@ def stage1():
         elif s1Target == 1:
             if vr.read() < 100 :
                 stage1SuccessFlag = 1
->>>>>>> Stashed changes
                 fan.duty(0)
                 break
-        
         delay(100)
-<<<<<<< Updated upstream
-            
-    
-##def stage1(): ##電阻控制風扇
-    
-    
-def stage2LCD():
-    global color
-    
-    colorList = ["Yellow", "Magenta", "Cyan", "White"]
-    picker = urandom.randint(0, 3)
-    color = colorList[picker]
-=======
 
 
 ##---Stage2 RGB顏色: 按鈕控制RGB---
@@ -274,14 +195,13 @@ def stage2Front():
 
     colorPicker = urandom.randint(0, 3)
     s2Color = s2ColorList[colorPicker]
->>>>>>> Stashed changes
     
 def stage2Display():
     global stageInstruction
     global s2Color
     global s2ColorList
     lcd.move_to(0,0)
-    lcd.putstr("change the light")
+    lcd.putstr("Change The Light")
     lcd.move_to(0,1)
     lcd.putstr("to " + s2Color)
 
@@ -295,22 +215,12 @@ def stage2Display():
     elif s2Color == s2ColorList[3]:
         stageInstruction[2] = "按下按鈕，讓燈變成白色"
     
-<<<<<<< Updated upstream
-def stage2(): ##按鈕RGB
-    global currentStageIndex
-    global stage2Success
-    global color
-    global rgbRIndex
-    global rgbGIndex
-    global rgbBIndex
-=======
 def stage2(): 
     global stage2SuccessFlag
     global s2Color
     rgbRIndex = 0
     rgbGIndex = 0
     rgbBIndex = 0
->>>>>>> Stashed changes
     
     while True:
         if btnR.value() == 1:
@@ -340,66 +250,48 @@ def stage2():
             
         if s2Color == s2ColorList[0]:
             if rgbRIndex == 1 & rgbGIndex == 1:
-                stage2Success = 1
+                stage2SuccessFlag = 1
+                delay(1000)
+                clearLED()
                 break
-<<<<<<< Updated upstream
-        if color == "Magenta":
-=======
         elif s2Color == s2ColorList[1]:
->>>>>>> Stashed changes
             if rgbRIndex == 1 & rgbBIndex == 1:
-                stage2Success = 1
+                stage2SuccessFlag = 1
+                delay(1000)
+                clearLED()
                 break
-<<<<<<< Updated upstream
-        if color == "Cyan":
-=======
         elif s2Color == s2ColorList[2]:
->>>>>>> Stashed changes
             if rgbGIndex == 1 & rgbBIndex == 1:
-                stage2Success = 1
+                stage2SuccessFlag = 1
+                delay(1000)
+                clearLED()
                 break
-<<<<<<< Updated upstream
-        if color == "White":
-=======
         elif s2Color == s2ColorList[3]:
->>>>>>> Stashed changes
             if rgbRIndex == 1 & rgbGIndex == 1 & rgbBIndex == 1:
-                stage2Success = 1
+                stage2SuccessFlag = 1
+                delay(1000)
+                clearLED()
                 break
     
-<<<<<<< Updated upstream
-    
-def stage3LCD():
-=======
 
 ##--- Stage3: 手指增加濕度 ---(標準為當下濕度，通關為當下+5或3)
 ##def stage3Front():
 
 def stage3Display():
->>>>>>> Stashed changes
     lcd.move_to(0,0)
     lcd.putstr("3")
     
-##def stage3(): ##手指增加濕度(標準為當下濕度，通關為當下+5或3)
+##def stage3():
     
     
-<<<<<<< Updated upstream
-def stage4LCD():
-=======
 ##--- Stage4: 光敏電阻 ---
 def stage4Front():
->>>>>>> Stashed changes
     lcd.move_to(0,0)
     lcd.putstr("4")
     
-##def stage4(): ##光敏
+##def stage4(): 
 
 
-<<<<<<< Updated upstream
-def stage5LCD():
-    lcd.move_to(0,0)
-    lcd.putstr("5")
-=======
 
 ##--- Stage5 藍芽模組解謎: 手機答題 ---
 def stage5Front():
@@ -446,29 +338,9 @@ def incorrect():
 def stage5():
     global s5Question
     global s5QuestionList
->>>>>>> Stashed changes
     
-def stage5(): ##藍芽模組解謎
     while True:
         cmd = str(ble.read(30), 'utf-8').strip('\0')
-<<<<<<< Updated upstream
-        if cmd != '':
-            if cmd == 'cc':
-                print('cc2')
-            elif cmd == 'lol':
-                print('lol2')
-        delay(10)
-
-##------------------------------------------------------------
-##def checkStage():
-##    lcd.clear()
-##    delay(500)
-##    lcd.move_to(0,0)
-##    lcd.putstr("Stage: " + str(currentStageIndex) + "/5")
-##    delay(500)
-##    currentStage(currentStageIndex)
-
-=======
         if is_ble_connected == True:
             if cmd != "":
                 print(cmd)
@@ -487,7 +359,6 @@ def stage5(): ##藍芽模組解謎
                 ##TODO: 有加題目就繼續放
 
         delay(1000)
->>>>>>> Stashed changes
 
 
 ##主要--------------------------------------------------------
@@ -525,24 +396,5 @@ def main():
 
 
 ##主程式------------------------------------------------------
-<<<<<<< Updated upstream
-startGame()
-'''
-currentStage(2)
-if stage2Success == 1:
-    nextStage()
-    clearLED()
-    currentStage(currentStageIndex)
-print(currentStageIndex)
-'''
-currentStage(1)
-if stage1Success == 1:
-    nextStage()
-    clearLED()
-    currentStage(currentStageIndex)
-print(currentStageIndex)
-        
-=======
 main()
 ##restart()
->>>>>>> Stashed changes
