d8edd53808d7f16cb62b75e43da4f34a06ad435d
Zypheus <c25887@gmail.com>
2022-07-18 16:06:12 +0800

    while BUG

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 ##插件--------------------------------------------------------
-from machine import I2C, Pin, ADC, PWM, Timer
+from machine import I2C, Pin, ADC, PWM
 from sensor import DHT11
 import voice_recognition
-from gpb import delay
+from gpb import delay, timer
 import urandom
 from ble_uart import BleUart
 from i2c_lcd import I2cLcd
 from lcd_api import LcdApi
 
-timer = Timer(1)
+##gameTime = timer(1) ##TODO: Timer不知道怎麼取得時間值QQ
 
 ##語音辨識用
 voice_recognition.load_database('cmd.bin')
@@ -38,11 +38,10 @@ while True:
         is_ble_connected = True
         break
 
-while is_ble_connected == True:
-    if cmd == "SYSTEM-ECHO=DISCONNECTED OK\r\n":
+if cmd == "SYSTEM-ECHO=DISCONNECTED OK\r\n":
+    if is_ble_connected == True:
         is_ble_connected = False
         disconnectedFlag = 1
-        break
 
 
 ##元件腳位----------------------------------------------------
@@ -107,20 +106,21 @@ def bleStageInstructionDisplay(index): ##藍芽: 手機上顯示題目
 
 
 def startGame():
-    global timer
+    ##global gameTime 
     global inGame
 
-    timer.init(period=500, mode=Timer.PERIODIC, callback=None) ##TODO: 計時器測試
+    ##gameTime.init(freq=1000) ##TODO: Timer不知道怎麼取得時間值QQ
     inGame = 1
 
     lcd.move_to(0,0)
     lcd.putstr("Game Start!!")
+    lcd.move_to(0,1)
+    lcd.putstr("Good Luck!")
     clearLCD2()
     
-    lcd.putstr("Good Luck!")
-    lcd.move_to(0,1)
+    lcd.move_to(0,0)
     lcd.putstr("Stage: 1/5")
-    ble.write("-------------Stage 1-------------")
+    ble.write("-----------Stage 1-----------")
     clearLCD2()
 
     currentStage()
@@ -164,7 +164,7 @@ def nextStage():
     lcd.putstr("Next Stage")
     lcd.move_to(0,1)
     lcd.putstr("Stage: " + str(currentStageIndex) + "/5")
-    ble.write("-------------Stage " + str(currentStageIndex) +"-------------")
+    ble.write("-----------Stage " + str(currentStageIndex) +"-----------")
     clearLCD2()
     
     currentStage()
@@ -187,23 +187,25 @@ def hint():
     ##else: ##TODO: 看題目是什麼, 給不同提示
 
 def finish(): ##關卡全破
-    global timer
+    ##global gameTime
 
     clearLCD1()
     lcd.move_to(0,0)
     lcd.putstr("Congratulations!")
-    ble.write("--------Congratulations!!--------")
-    lcd.move_to(0,1)
-    lcd.putstr("time: " + str(timer))
+    ble.write("------Congratulations!!------")
+    ##lcd.move_to(0,1)
+    ##lcd.putstr("time: " + str(gameTime)) ##TODO: Timer不知道怎麼取得時間值QQ
 
 def gameover(): ##強制結束遊戲
-    global timer
+    ##global gameTime
 
     lcd.move_to(0,0)
     lcd.putstr("Game Over")
-    ble.write("------------Game Over------------")
-    lcd.move_to(0,1)
-    lcd.putstr("time: " + str(timer))
+    ble.write("----------Game Over----------")
+    ##lcd.move_to(0,1)
+    ##lcd.putstr("time: " + str(gameTime)) ##TODO: Timer不知道怎麼取得時間值QQ
+
+    init()
 
 def disconnected():
     lcd.move_to(0,0)
@@ -220,7 +222,7 @@ def main(): ##遊戲開始與推進用
     global stage5SuccessFlag
 
     if is_ble_connected == True:
-        ##currentStageIndex = 1 ##TODO:Debug時指定起始關卡用
+        currentStageIndex = 5 ##TODO:Debug時指定起始關卡用
         startGame()
         if stage1SuccessFlag == 1:
             nextStage()
@@ -234,7 +236,7 @@ def main(): ##遊戲開始與推進用
             finish()
 
 def init(): ##初始化/重置
-    global timer
+    ##global gameTime
     global inGame
     global disconnectedFlag
     global currentStageIndex
@@ -254,7 +256,7 @@ def init(): ##初始化/重置
     global s4Target
     global s5Question
 
-    timer.deinit()
+    ##gameTime.deinit() ##TODO: Timer不知道怎麼取得時間值QQ
     inGame = 0
     disconnectedFlag = 0
     currentStageIndex = 1
@@ -273,8 +275,6 @@ def init(): ##初始化/重置
     s4TargetFlag = 0
     s4Target = 0
     s5Question = ""
-
-    clearLCD2()
 
 
 ##各關函式----------------------------------------------------
@@ -300,7 +300,7 @@ def stage1Display():
 
     #bleDisplay
     if s1TargetFlag == 1:
-        stageInstruction[1] = "讓扇轉快一點"
+        stageInstruction[1] = "讓風扇轉快一點"
     else:
         stageInstruction[1] = "讓風扇停下來"
     
@@ -310,19 +310,19 @@ def stage1():
     global s1TargetFlag
 
     while True:
+        delay(1000)
         print(vr.read())
         fan.duty(int(vr.read()/4))
         if s1TargetFlag == 1:
-            if vr.read() < 100 :
+            if vr.read() < 300 :
                 stage1SuccessFlag = 1
                 fan.duty(0)
                 break
         else:
-            if vr.read() > 3600:
+            if vr.read() > 3500:
                 stage1SuccessFlag = 1
                 fan.duty(0)
                 break
-        delay(100)
 
 
 ##---Stage2: 按鈕控制RGB顏色---
@@ -419,8 +419,16 @@ def stage3Front():
     global dht
     global s3Target
 
-    s3Target = dht.humidity() + 5
+    while True:
+        humidity = dht.humidity()
+        delay(300)
+        print(humidity)
+        if humidity >= 5:
+            if humidity <= humidity <= 63:
+                break
+    s3Target = humidity + 5
     print(s3Target)
+    print("-----")
 
 def stage3Display():
     lcd.move_to(0,0)
@@ -442,19 +450,27 @@ def stage3():
             break
     
 
-##--- Stage4: 光敏電阻 --- ##TODO: 合併完, 要測試
+##--- Stage4: 光敏電阻 ---
 def stage4Front():
     global s4Brightness
     global s4TargetFlag
     global s4Target
     
-    s4Brightness = lightSensor.read()
+    while True:
+        s4Brightness = lightSensor.read()
+        delay(300)
+        print(s4Brightness)
+        if s4Brightness > 100:
+            break
     s4TargetFlag = urandom.randint(0,1)
+    print(s4TargetFlag)
+    print("-----")
 
     if s4TargetFlag == 1:
-        s4Target = s4Brightness + 100
+        s4Target = s4Brightness - 300 ##越亮, 值越低
     else:
-        s4Target = s4Brightness - 50
+        s4Target = s4Brightness + 300
+    print(s4Target)
 
 def stage4Display():
     global s4TargetFlag
@@ -463,10 +479,10 @@ def stage4Display():
     lcd.move_to(0,0)
     if s4TargetFlag == 1:
         lcd.putstr("Let it Brighter")
-        stageInstruction[4] = "讓他變得更亮"
+        stageInstruction[4] = "讓光感電阻變得更亮"
     else:
         lcd.putstr("Let it Darker")
-        stageInstruction[4] = "讓他變得更暗"
+        stageInstruction[4] = "讓光感電阻變得更暗"
     
 def stage4():
     global s4Brightness
@@ -475,14 +491,19 @@ def stage4():
     global stage4SuccessFlag
 
     while True:
-        s4Brightness = lightSensor.read()
-        print(str(s4Brightness))
+        delay(1000)
+        brightnessRead = lightSensor.read()
+        if brightnessRead != 0:
+            s4Brightness = brightnessRead
+        print(s4Brightness)
         if s4TargetFlag == 1:
+            if s4Brightness < s4Target: ##越亮, 值越低
+                stage4SuccessFlag = 1
+                break
+        else:
             if s4Brightness > s4Target:
                 stage4SuccessFlag = 1
-        else:
-            if s4Brightness < s4Target:
-                stage4SuccessFlag = 1
+                break
 
 
 ##--- Stage5: 手機解謎(藍芽) ---
@@ -501,7 +522,7 @@ def stage5Display():
     lcd.putstr("Answer on Phone")
 
     ##bleDisplay
-    stageInstruction[5] = "請回答下列問題: " + s5Question
+    stageInstruction[5] = "請回答下列問題:\r\n" + s5Question
     
 def stage5():
     global s5Question
@@ -528,19 +549,42 @@ def stage5():
 
 
 ##主程式------------------------------------------------------
+
+##迴圈不會執行的樣子
 while True:
-    cmd_id = voice_recognition.get_id() ##TODO: 安裝語音包, 確認語音ID
+    cmd_id = voice_recognition.get_id() ##TODO: 語音包
     if inGame == 0: ##未進遊戲
         if cmd_id == 1: ##開始遊戲
             init()
             main()
+            print(cmd_id)
     else: ##正在遊戲中
         if cmd_id == 2: ##結束遊戲
             gameover()
+            print(cmd_id)
         if cmd_id == 3: ##請求支援(提示)
             hint()
-        if cmd_id == 7: ##重新遊戲
+            print(cmd_id)
+        if cmd_id == 7: ##重新開始
             init()
             main()
+            print(cmd_id)
         if disconnectedFlag == 1:
             disconnected()
+
+
+'''
+##閹割版
+while True:
+    cmd_id = voice_recognition.get_id() ##TODO: 語音包
+    if cmd_id == 1: ##開始遊戲
+        init()
+        main()
+        print(cmd_id)
+    if cmd_id == 7: ##重新開始
+        init()
+        main()
+        print(cmd_id)
+    if disconnectedFlag == 1:
+            disconnected()
+'''
