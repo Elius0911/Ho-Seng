0f662ed34c7f5a8ac0ea8a19d8a5273d9be61a16
Zypheus <c25887@gmail.com>
2022-07-18 19:51:16 +0800

    voiceCMD + conn. + disconn.

--- boot.py
+++ boot.py
@@ -1,5 +1,5 @@
 ##插件--------------------------------------------------------
-from machine import I2C, Pin, ADC, PWM, 
+from machine import I2C, Pin, ADC, PWM
 from sensor import DHT11
 import voice_recognition
 from gpb import delay, timer
@@ -33,19 +33,6 @@ ble.cmd_mode_entry()
 ble.cmd_AT()
 ble.set_device_name('Ho-Seng')
 
-while True:
-    cmd = str(ble.read(30), 'utf-8').strip('\0')
-    delay(1000)
-    if cmd == "SYSTEM-ECHO=CONNECTED OK\r\n":
-        ble.data_mode_entry()
-        is_ble_connected = True
-        break
-
-while is_ble_connected == True:
-    if cmd == "SYSTEM-ECHO=DISCONNECTED OK\r\n":
-        is_ble_connected = False
-        disconnectedFlag = 1
-        break
 
 
 ##元件腳位----------------------------------------------------
@@ -66,8 +53,8 @@ lightSensor = ADC(0)
 
 ##全域變數----------------------------------------------------
 inGame = 0 ##是否正在遊戲中
-disconnectedFlag = 0
 currentStageIndex = 1
+disconnectedFlag = 0
 stageInstruction = ["", "Instuction1", "Instuction2", "Instuction3", "Instuction4", "Instuction5"]  ##關卡說明
 
 stage1SuccessFlag = 0
@@ -112,15 +99,22 @@ def bleStageInstructionDisplay(index): ##藍芽: 手機上顯示題目
 ##起始字樣
 def boot():
     lcd.move_to(0,0)
+    lcd.putstr("Made by FongShan")
+    lcd.move_to(0,1)
+    lcd.putstr("New Age Men")
+    clearLCD2()
+
+    lcd.move_to(0,0)
     lcd.putstr("Welcome to")
     lcd.move_to(0,1)
     lcd.putstr("Ho-Seng")
     clearLCD2()
 
     lcd.move_to(0,0)
-    lcd.putstr("Made by FongShan")
+    lcd.putstr("Waiting For")
     lcd.move_to(0,1)
-    lcd.putstr("New Age Men")
+    lcd.putstr("BLE Connect")
+
 
 def startGame():
     ##global gameTime ##TODO: Timer
@@ -131,10 +125,10 @@ def startGame():
 
     lcd.move_to(0,0)
     lcd.putstr("Game Start!!")
-    clearLCD2()
-    
-    lcd.putstr("Good Luck!")
     lcd.move_to(0,1)
+    lcd.putstr("Good Luck!")
+    clearLCD2()
+
     lcd.putstr("Stage: 1/5")
     ble.write("-----------Stage 1-----------")
     clearLCD2()
@@ -203,7 +197,10 @@ def hint():
     ##else: ##TODO: 看題目是什麼, 給不同提示
 
 def allClear(): ##關卡全破
+    global inGame
     ##global gameTime ##TODO: Timer
+
+    inGame = 0
 
     clearLCD1()
     lcd.move_to(0,0)
@@ -213,15 +210,38 @@ def allClear(): ##關卡全破
     ##lcd.putstr("time: " + str(timer)) ##TODO: Timer
 
 def finishGame(): ##強制結束遊戲
+    global inGame
     ##global gameTime ##TODO: Timer
 
+    inGame = 0
+
+    clearLCD1()
     lcd.move_to(0,0)
     lcd.putstr("Game Over")
     ble.write("----------Game Over----------")
     ##lcd.move_to(0,1)
     ##lcd.putstr("time: " + str(timer)) ##TODO: Timer
 
+def connected():
+    global disconnectedFlag
+
+    disconnectedFlag = 0
+    
+    clearLCD1()
+    lcd.move_to(0,0)
+    lcd.putstr("Connected")
+    clearLCD2()
+
+    lcd.move_to(0,0)
+    lcd.putstr("Wait For Voice")
+    lcd.move_to(0,1)
+    lcd.putstr("CMD to Start")
+
 def disconnected():
+    global disconnectedFlag
+
+    disconnectedFlag = 1
+    clearLCD2()
     lcd.move_to(0,0)
     lcd.putstr("Disconnected")
 
@@ -236,22 +256,22 @@ def main(): ##遊戲開始與推進用
     global stage5SuccessFlag
 
     if is_ble_connected == True:
-        ##currentStageIndex = 1 ##TODO:Debug時指定起始關卡用
+        currentStageIndex = 1 ##TODO:Debug時指定起始關卡用
         startGame()
         if stage1SuccessFlag == 1:
-            audio_decode.start('complete.mp3')
+            audio_decode.start('correct.mp3')
             nextStage()
         if stage2SuccessFlag == 1:
-            audio_decode.start('complete.mp3')
+            audio_decode.start('correct.mp3')
             nextStage()
         if stage3SuccessFlag == 1:
-            audio_decode.start('complete.mp3')
+            audio_decode.start('correct.mp3')
             nextStage()
         if stage4SuccessFlag == 1:
-            audio_decode.start('complete.mp3')
+            audio_decode.start('correct.mp3')
             nextStage()
         if stage5SuccessFlag == 1:
-            audio_decode.start('complete.mp3')
+            audio_decode.start('allClear.mp3') ##TODO: 放ALL CLEAR音樂
             allClear()
 
 def init(): ##初始化/重置
@@ -297,18 +317,6 @@ def init(): ##初始化/重置
 
     clearLCD2()
 
-def voiceCMD():
-    cmd_id = voice_recognition.get_id()
-    if cmd_id == 2: ##結束遊戲
-        finishGame()
-    if cmd_id == 3: ##請求支援(提示)
-        hint()
-    if cmd_id == 7: ##重新遊戲
-        init()
-        main()
-    if disconnectedFlag == 1:
-        disconnected()
-
 
 ##各關函式----------------------------------------------------
 
@@ -341,9 +349,27 @@ def stage1():
     global stage1SuccessFlag
     global vr
     global s1TargetFlag
+    global is_ble_connected
 
     while True:
-        voiceCMD()
+        cmd_id = voice_recognition.get_id()
+        if inGame == 1:
+            if cmd_id == 2: ##結束遊戲
+                finishGame()
+                break
+            if cmd_id == 3: ##請求支援(提示)
+                hint()
+            if cmd_id == 7: ##重新遊戲
+                init()
+                main()
+                break
+        
+        cmd = str(ble.read(30), 'utf-8').strip('\0')
+        delay(500)
+        if cmd == "SYSTEM-ECHO=DISCONNECTED OK\r\n":
+            is_ble_connected = False
+            disconnected()
+            break
 
         fan.duty(int(vr.read()/4))
         if s1TargetFlag == 1:
@@ -389,12 +415,31 @@ def stage2Display():
 def stage2(): 
     global stage2SuccessFlag
     global s2Color
+    global is_ble_connected
+
     rgbRIndex = 0
     rgbGIndex = 0
     rgbBIndex = 0
-    
+
     while True:
-        voiceCMD()
+        cmd_id = voice_recognition.get_id()
+        if inGame == 1:
+            if cmd_id == 2: ##結束遊戲
+                finishGame()
+                break
+            if cmd_id == 3: ##請求支援(提示)
+                hint()
+            if cmd_id == 7: ##重新遊戲
+                init()
+                main()
+                break
+        
+        cmd = str(ble.read(30), 'utf-8').strip('\0')
+        delay(500)
+        if cmd == "SYSTEM-ECHO=DISCONNECTED OK\r\n":
+            is_ble_connected = False
+            disconnected()
+            break
 
         ##按鈕偵測
         if btnR.value() == 1:
@@ -456,10 +501,16 @@ def stage3Front():
 
     while True:
         humidity = dht.humidity()
+        print(humidity)
         delay(200)
         if humidity >= 5:
-            if humidity <= humidity <= 63:
+            if humidity <= 65:
                 break
+            else:
+                humidity = 60
+        else:
+            humidity = 60
+        
     s3Target = humidity + 5
 
 def stage3Display():
@@ -472,8 +523,28 @@ def stage3Display():
 def stage3():
     global dht
     global stage3SuccessFlag
+    global is_ble_connected
 
     while True:
+        cmd_id = voice_recognition.get_id()
+        if inGame == 1:
+            if cmd_id == 2: ##結束遊戲
+                finishGame()
+                break
+            if cmd_id == 3: ##請求支援(提示)
+                hint()
+            if cmd_id == 7: ##重新遊戲
+                init()
+                main()
+                break
+
+        cmd = str(ble.read(30), 'utf-8').strip('\0')
+        delay(500)
+        if cmd == "SYSTEM-ECHO=DISCONNECTED OK\r\n":
+            is_ble_connected = False
+            disconnected()
+            break
+
         humidity = dht.humidity()
         if humidity >= s3Target:
             stage3SuccessFlag = 1
@@ -498,7 +569,6 @@ def stage4Front():
         s4Target = s4Brightness - 300 ##越亮, 值越低
     else:
         s4Target = s4Brightness + 300
-    print(s4Target)
 
 def stage4Display():
     global s4TargetFlag
@@ -517,13 +587,31 @@ def stage4():
     global s4TargetFlag
     global s4Target
     global stage4SuccessFlag
+    global is_ble_connected
 
     while True:
+        cmd_id = voice_recognition.get_id()
+        if inGame == 1:
+            if cmd_id == 2: ##結束遊戲
+                finishGame()
+                break
+            if cmd_id == 3: ##請求支援(提示)
+                hint()
+            if cmd_id == 7: ##重新遊戲
+                init()
+                main()
+                break
+        
+        cmd = str(ble.read(30), 'utf-8').strip('\0')
+        delay(500)
+        if cmd == "SYSTEM-ECHO=DISCONNECTED OK\r\n":
+            is_ble_connected = False
+            disconnected()
+            break
         
         brightnessRead = lightSensor.read()
         if brightnessRead != 0:
             s4Brightness = brightnessRead
-        print(s4Brightness)
         if s4TargetFlag == 1:
             if s4Brightness < s4Target: ##越亮, 值越低
                 stage4SuccessFlag = 1
@@ -551,40 +639,79 @@ def stage5Display():
     lcd.putstr("Answer on Phone")
 
     ##bleDisplay
-    stageInstruction[5] = "請回答下列問題: " + s5Question
+    stageInstruction[5] = "請回答下列問題\r\n" + s5Question
     
 def stage5():
     global s5Question
     global stage5SuccessFlag
-    
+    global is_ble_connected
+
     while True:
-        voiceCMD()
+        cmd_id = voice_recognition.get_id()
+        if inGame == 1:
+            if cmd_id == 2: ##結束遊戲
+                finishGame()
+                break
+            if cmd_id == 3: ##請求支援(提示)
+                hint()
+            if cmd_id == 7: ##重新遊戲
+                init()
+                main()
+                break
+
         cmd = str(ble.read(30), 'utf-8').strip('\0')
-        if cmd != "":
+        delay(500)
+        if cmd == "SYSTEM-ECHO=DISCONNECTED OK\r\n":
+            is_ble_connected = False
+            disconnected()
+            break
+        elif cmd != "":
             if cmd == s5Question:
                 clearLCD1()
                 lcd.move_to(0,0)
                 lcd.putstr("Correct")
+                audio_decode.start('correct.mp3')
                 clearLCD1()
                 stage5SuccessFlag = 1
                 break
             else:
                 clearLCD1()
                 lcd.move_to(0,0)
+                audio_decode.start('wrong.mp3')
                 lcd.putstr("Incorrect")
                 clearLCD1()
                 stage5Display()
-        delay(500)
+        delay(200)
 
 
 ##主程式------------------------------------------------------
 boot()
+
+while True:
+    cmd = str(ble.read(30), 'utf-8').strip('\0')
+    delay(500)
+    if cmd == "SYSTEM-ECHO=CONNECTED OK\r\n":
+        ble.data_mode_entry()
+        is_ble_connected = True
+        connected()
+        break
+
 while True:
     cmd_id = voice_recognition.get_id()
+    cmd = str(ble.read(30), 'utf-8').strip('\0')
     if inGame == 0: ##未進遊戲
         if cmd_id == 1: ##開始遊戲
             init()
             main()
-        if cmd_id == 7: ##重新遊戲
+        if cmd_id == 7: ##重新開始
             init()
             main()
+    delay(500)
+    if cmd == "SYSTEM-ECHO=DISCONNECTED OK\r\n":
+        is_ble_connected = False
+        disconnected()
+    if disconnectedFlag == 1:
+        if cmd == "SYSTEM-ECHO=CONNECTED OK\r\n":
+            ble.data_mode_entry()
+            is_ble_connected = True
+            connected()
